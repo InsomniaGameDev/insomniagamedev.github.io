6a47430b5c04eb9014eabe048c6a7f3953cd359e
Elias <eliasvaz70@gmail.com>
2017-07-24 17:46:18 -0300

    Version 2.08
    Ajuste no layout nas janelas, mudança no menu e novos icones para redes
    sociais.

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,78 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+$(document).ready(function () {
+    "use strict";
+  // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+              // Using jQuery's animate() method to add smooth page scroll
+              // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
 function openNav() {
     "use strict";
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidenav").style.width = "65px";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
     "use strict";
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
 }
 
 function openWindow() {
     "use strict";
-    document.getElementById("card-efemero").style.display = "flex";
+    document.getElementById("card-efemero").style.visibility = "visible";
+    document.getElementById("card-efemero").style.opacity = "1";
 }
 
 function closeWindow() {
     "use strict";
-    document.getElementById("card-efemero").style.display = "none";
+    document.getElementById("card-efemero").style.opacity = "0";
+    document.getElementById("card-efemero").style.visibility = "hidden";
 }
+
+var slideIndex = 1;
+    showSlides(slideIndex);
+
+function plusSlides(n) {
+    "use strict";
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    "use strict";
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    "use strict";
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1; }
+    if (n < 1) {slideIndex = slides.length; }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+  
